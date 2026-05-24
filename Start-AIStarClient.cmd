6630a87d273e6af358e698fe76ba48730ba1d70d
NeoMind <dwj1996@naver.com>
2026-05-24 23:44:09 +0900

    fix: refresh local start script build

--- Start-AIStarClient.cmd
+++ Start-AIStarClient.cmd
@@ -6,9 +6,7 @@ set "APPDIR=%~dp0artifacts\run\AIStarClient-%VERSION%"
 set "BUILDDIR=%~dp0artifacts\run-build\AIStarClient-%VERSION%"
 set "APP=%APPDIR%\StarAI.PracticeClient.App.exe"
 
-if not exist "%APP%" (
-  dotnet publish "%~dp0src\StarAI.PracticeClient.App\StarAI.PracticeClient.App.csproj" -c Release -o "%APPDIR%" -p:OutputPath="%BUILDDIR%\"
-  if errorlevel 1 exit /b %errorlevel%
-)
+dotnet publish "%~dp0src\StarAI.PracticeClient.App\StarAI.PracticeClient.App.csproj" -c Release -o "%APPDIR%" -p:OutputPath="%BUILDDIR%\"
+if errorlevel 1 exit /b %errorlevel%
 
 start "AIStarClient" "%APP%"
